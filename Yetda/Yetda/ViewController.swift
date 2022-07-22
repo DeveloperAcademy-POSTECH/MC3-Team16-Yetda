c37c307da297c9c97268782da57f384107cf2c26
rbwo552 <rbwo552@naver.com>
2022-07-22 16:16:46 +0900

    fix conflict

---
+++ Yetda/Yetda/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Yetda
+//
+//  Created by Geunil Park on 2022/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
